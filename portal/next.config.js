f80a7da4f5ede7e4dfc28c11f7cc5334f1746305
Danubio Coelho <104590059+danub-io@users.noreply.github.com>
2026-03-09 13:35:55 -0300

    Arch: Separando LP (raiz) e Portal (/portal)

--- portal/next.config.js
+++ portal/next.config.js
@@ -2,13 +2,10 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
-
 module.exports = {
   ...withNextra(),
   output: 'export',
-  basePath: '/mbalite',
-  assetPrefix: '/mbalite',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/mbalite/portal',
+  assetPrefix: '/mbalite/portal',
+  images: { unoptimized: true }
 }
