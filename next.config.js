642bffc3093afee10ef9fb969d1f04b2ee2c67e8
Danubio Coelho <104590059+danub-io@users.noreply.github.com>
2026-03-09 13:19:47 -0300

    Add GitHub Actions workflow for Next.js deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 ﻿const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
