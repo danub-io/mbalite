29dcbde8e826a9dca88fcee505dd8eeb632d705c
Danubio Coelho <104590059+danub-io@users.noreply.github.com>
2026-03-09 17:58:06 -0300

    fix: ajuste de links relativos e correcao de codificacao na landing page

--- portal/next.config.js
+++ portal/next.config.js
@@ -1,4 +1,4 @@
-const withNextra = require('nextra')({
+﻿const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
@@ -7,6 +7,7 @@ module.exports = {
   output: 'export',
   basePath: '/mbalite/portal',
   assetPrefix: '/mbalite/portal',
+  trailingSlash: true,
   images: { unoptimized: true },
   trailingSlash: true
 }
