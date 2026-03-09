0b9355c9e4352390ecd2c5d954d19e0cbc83a3de
Danubio Coelho <104590059+danub-io@users.noreply.github.com>
2026-03-09 18:30:43 -0300

    fix: remove github icon, reestrutura index portal e limpeza profunda de texto

--- portal/theme.config.tsx
+++ portal/theme.config.tsx
@@ -4,9 +4,7 @@ import LogoutButton from './components/LogoutButton'
 
 const config: DocsThemeConfig = {
   logo: <span style={{ fontWeight: 900, fontSize: '1.25rem', letterSpacing: '-0.05em' }}>🚀 MBA<span style={{color: '#6366f1'}}>Lite.</span></span>,
-  project: {
-    link: 'https://github.com/danub-io/mbalite',
-  },
+  
   navbar: {
     extraContent: <LogoutButton />
   },
